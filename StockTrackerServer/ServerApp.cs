b58bbd0623e37e451fa2cda5e16b21d4755da326
Nathan Parnell <nparnell@redev.co.uk>
2023-09-29 21:53:06 +0100

    Tidied up ServerApp.cs as it had lots of unused commented code which had no value

--- StockTrackerServer/ServerApp.cs
+++ StockTrackerServer/ServerApp.cs
@@ -14,84 +14,23 @@ namespace StockTrackerServer
 {
     public class ServerApp
     {
-        private readonly IDataService _dataService;
-        private readonly IServerTransportService _transportServiceServer;
+        private readonly IServerTransportService _transportServiceServer;   
         
-        
-        public ServerApp(IDataService dataService, IServerTransportService transportServiceServer) 
+        public ServerApp(IServerTransportService transportServiceServer) 
         {
-            _dataService = dataService;
             _transportServiceServer = transportServiceServer;
         }
 
+        /// <summary>
+        /// Method called from Program.cs which actually starts the listener thread
+        /// </summary>
+        /// <param name="args"></param>
         public void Run(string[] args)
         {
             Console.WriteLine("SERVER--------");
 
             Thread listenerThread = new Thread(() => _transportServiceServer.ListenThread());
             listenerThread.Start();
-
-            //IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-            //Console.WriteLine("Starting TCP Listener...");
-            //TcpListener listener = new TcpListener(ipAddress, 5000);
-            //listener.Start();
-
-            //try
-            //{
-            //    while (true)
-            //    {
-            //        var tcpClient = listener.AcceptTcpClient();
-
-
-            //        StreamReader inStream = new StreamReader(tcpClient.GetStream(), Encoding.ASCII);
-
-
-            //        string receivedData = inStream.ReadLine();
-            //        Console.WriteLine("Received: " + receivedData);
-            //        //Socket clientSocket = listener.AcceptSocket();
-            //        //Console.WriteLine("Connection accepted.");
-
-            //        //var childSocketThread = new Thread(() =>
-            //        //{
-            //        //    byte[] data = new byte[100];
-            //        //    int size = clientSocket.Receive(data);
-            //        //    Console.WriteLine("Recieved data: ");
-            //        //    string x = "";
-            //        //    for (int i = 0; i < size; i++)
-            //        //    {
-            //        //        //x.Append(Convert.ToChar(data[i]));
-            //        //        x = Encoding.ASCII.GetString(data);
-            //        //    }
-
-            //        //    Console.Write(x);
-
-            //        //    Console.WriteLine();
-            //        //    clientSocket.Send(Encoding.ASCII.GetBytes($"Message {x.ToString()} Recieved"));
-
-            //        //    clientSocket.Close();
-            //        //});
-
-            //        //childSocketThread.Start();
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("Error: " + e.Message);
-            //}
-            //finally
-            //{
-            //    listener.Stop(); // Stop the listener when done
-            //}
-
         }
-
     }
 }
-
-
-//code which prints out users
-//User[] users = _dataService.GetAllUsers().Result.ToArray();
-//foreach(User user in users)
-//{
-//    Console.WriteLine($"User : {user.UserName}");
-//}

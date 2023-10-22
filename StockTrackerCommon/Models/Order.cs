6718d4656c71abc293fdeeb0224feb074117ba09
Nathan Parnell <nparnell@redev.co.uk>
2023-10-22 18:29:53 +0100

    Created order class #17

---
+++ StockTrackerCommon/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrackerCommon.Models
+{
+    public class Order
+    {
+        public Order() { } 
+        public string OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public string SupplierId { get; set; }
+        public string ProductId { get; set; }
+        public string Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string OrderNotes { get; set; }
+    }
+}

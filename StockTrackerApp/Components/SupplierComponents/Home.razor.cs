07667385df14f064155f21064819e2faa7b5b0fd
Nathan Parnell <nparnell@redev.co.uk>
2023-11-15 20:18:33 +0000

    Added sort by to supplier homepage, allowing suppliers to sort their products based of things like quantity, product name and price #8

--- StockTrackerApp/Components/SupplierComponents/Home.razor.cs
+++ StockTrackerApp/Components/SupplierComponents/Home.razor.cs
@@ -31,12 +31,65 @@ namespace StockTrackerApp.Components.SupplierComponents
         private Product _selectedProduct = new Product();
         private bool _isProductSelected = false;
 
+        private List<String> _sortByOptions = new List<String>() { "Product Name (A-Z)", "Product Name (Z-A)", "Product Brand (A-Z)", "Product Brand (Z-A)", "Price (Low to High)", "Price (High to Low)",
+            "Quantity (Low to High)", "Quantity (High to Low)"};
+
         //private List<string> _selectedCategories = new List<string>();
 
         protected override async Task OnInitializedAsync()
         {
             _sessionHistoryService.AddWebpageToHistory("Home");
             GetProductsInformation();
+        }
+
+        /// <summary>
+        /// Sorts the products based on the selected option and re-renders the page
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private async Task SortProducts(ChangeEventArgs e)
+        {
+            SortProducts(e.Value.ToString());
+            StateHasChanged();
+        }
+
+        /// <summary>
+        /// Sorts the products based on the selected option
+        /// </summary>
+        /// <param name="sortByOption"></param>
+        /// <returns></returns>
+        private void SortProducts (string sortByOption)
+        {
+            //Sort the products based on the selected option
+            switch (sortByOption)
+            {
+                case "Product Name (A-Z)":
+                    _products = _products.OrderBy(prod => prod.ProductName).ToList();
+                    break;
+                case "Product Name (Z-A)":
+                    _products = _products.OrderByDescending(prod => prod.ProductName).ToList();
+                    break;
+                case "Product Brand (A-Z)":
+                    _products = _products.OrderBy(prod => prod.ProductBrand).ToList();
+                    break;
+                case "Product Brand (Z-A)":
+                    _products = _products.OrderByDescending(prod => prod.ProductBrand).ToList();
+                    break;
+                case "Price (Low to High)":
+                    _products = _products.OrderBy(prod => prod.Price).ToList();
+                    break;
+                case "Price (High to Low)":
+                    _products = _products.OrderByDescending(prod => prod.Price).ToList();
+                    break;
+                case "Quantity (Low to High)":
+                    _products = _products.OrderBy(prod => prod.ProductQuantity).ToList();
+                    break;
+                case "Quantity (High to Low)":
+                    _products = _products.OrderByDescending(prod => prod.ProductQuantity).ToList();
+                    break;
+                default:
+                    break;
+            }
         }
 
         private async Task GetProductsInformation()
@@ -47,6 +100,9 @@ namespace StockTrackerApp.Components.SupplierComponents
             //Get a list of the product category ids
             List<string> productCategoryIds = _products.Select(cat => cat.ProductCategoryId.ToString()).Distinct().ToList();
             _productCategories = _productCategoryService.GetProductCategoriesByProductCategoryIds(productCategoryIds);
+
+            //we need to sort the products by name
+            SortProducts(_sortByOptions[0]);
         }
 
         private void RowSelected(Product product)

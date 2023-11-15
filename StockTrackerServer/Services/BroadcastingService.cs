945161b6f1be539cc7b9ee73a3ce94ba9e0ca1f1
Nathan Parnell <nparnell@redev.co.uk>
2023-11-15 16:08:38 +0000

    Fixed broadcasting issue by making the oublished socket global #28

--- StockTrackerServer/Services/BroadcastingService.cs
+++ StockTrackerServer/Services/BroadcastingService.cs
@@ -14,26 +14,31 @@ namespace StockTrackerServer.Services
 {
     public class BroadcastingService : IBroadcastingService
     {
+        private readonly PublisherSocket _publisherSocket;
+
+        public BroadcastingService()
+        {
+            _publisherSocket = new PublisherSocket();
+            _publisherSocket.Bind("tcp://*:5557");
+        }
+
         public bool BroadcastMessage(Broadcast broadcastMessage)
         {
-            using (var publisherSocket = new PublisherSocket())
+            try
             {
-                //bind to our publishing socket
-                publisherSocket.Bind("tcp://*:5557");
-
                 //convert the broadcast message to json
-                string unencryptedBroadcast= JsonSerializer.Serialize(broadcastMessage);
+                string unencryptedBroadcast = JsonSerializer.Serialize(broadcastMessage);
 
                 //encrypt the broadcast message
                 string encryptedBroadcast = EncryptionHelper.Encrypt(unencryptedBroadcast);
 
                 //send the broadcast message
-                publisherSocket.SendMoreFrame("Notification").SendFrame(encryptedBroadcast);
-
-                //unbind from our publishing socket
-                publisherSocket.Unbind("tcp://*:5557");
-
+                _publisherSocket.SendMoreFrame("Notification").SendFrame(encryptedBroadcast);
                 return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
             }
         }
     }

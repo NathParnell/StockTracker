066069386562bd39a61d86452bc2660c85d2fffa
Nathan Parnell <nparnell@redev.co.uk>
2023-11-20 20:15:41 +0000

    Added project for Stock Tracker Common tests and added encryption and hashing helper tests #34

---
+++ StockTrackerCommonTests/Helpers/EncryptionHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrackerCommonTests.Helpers
+{
+    public class EncryptionHelperTests
+    {
+        [Fact]
+        public void testEncryptingString()
+        {
+            //Arrange
+            string unencryptedText = "textToEncrypt";
+
+            //Act
+            string encryptedText = EncryptionHelper.Encrypt(unencryptedText);
+
+            //Assert
+            //check the encrypted text is not null
+            Assert.NotNull(encryptedText);
+            //check the encrypted text is not the same as the unencrypted text
+            Assert.NotEqual(unencryptedText, encryptedText);
+        }
+
+        [Fact]
+        public void testDecryptingString()
+        {
+            //Arrange
+            string unencryptedText = "textToDecrypt";
+
+            //Act
+            string encryptedText = EncryptionHelper.Encrypt(unencryptedText);
+            string decryptedText = EncryptionHelper.Decrypt(encryptedText);
+
+            //Assert
+            //check the decrypted text is not null
+            Assert.NotNull(decryptedText);
+            //check the decrypted text is the same as the unencrypted text
+            Assert.Equal(unencryptedText, decryptedText);
+            //check the decrypted text is not the same as the encrypted text
+            Assert.NotEqual(encryptedText, decryptedText);
+        }
+    }
+}
---
+++ StockTrackerCommonTests/Helpers/HashingHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrackerCommonTests.Helpers
+{
+    public class HashingHelperTests
+    {
+        [Fact]
+        public void testHashing()
+        {
+            //Arrange
+            string stringToHash = "test";
+            string hashSalt = HashingHelper.GenerateHashSalt();
+
+            //Act
+            string hashedString1 = HashingHelper.Hasher(stringToHash, hashSalt);
+            string hashedString2 = HashingHelper.Hasher(stringToHash, hashSalt);
+
+            //Assert
+            //check the hashed strings are not null
+            Assert.NotNull(hashedString1);
+            Assert.NotNull(hashedString2);
+            //check the hashed strings are not the same as the unhashed string
+            Assert.NotEqual(stringToHash, hashedString1);
+            Assert.NotEqual(stringToHash, hashedString2);
+            //check the hashed strings are the same
+            Assert.Equal(hashedString1, hashedString2);
+        }
+    }
+}
